6effeded081ebbb7db87ae19608322190012c18d
sunss <sss213018@163.com>
2021-11-04 20:42:30 +0800

    🎉 init

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    borderWidth: {
+      DEFAULT: '1px',
+      0: '0',
+      2: '2px',
+      3: '3px',
+      4: '4px',
+      6: '6px',
+      8: '8px',
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
